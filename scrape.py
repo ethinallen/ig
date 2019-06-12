d5edb923571d6774a07f4b900e66a64abd66cab0
Drew Emery <abemery@clemson.edu>
2019-06-12 11:05:57 -0400

    site scrolls now, parsing the html to see how if all pictures are loaded

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 # libraries
+import time
 import requests
 import json
 from sys import argv
@@ -10,7 +11,10 @@ from bs4 import BeautifulSoup
 baseURL = 'https://instagram.com/'
 
 # the json of the person we look up
-person = {}
+# private by default
+person = {
+'public' : 0
+}
 
 # make a driver
 def makeDriver():
@@ -21,6 +25,17 @@ def makeDriver():
 def navigate(driver, destination):
     driver.get(destination)
 
+def loadAllPictures(driver):
+    for i in range(0,10):
+        # driver.find_by({div: class='v1Nh3 kIKUG  _bz0w'})
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(1.25)
+    html = driver.page_source
+    # soup = BeautifulSoup(html, html.parser)
+    print(html)
+
+
+
 # pull all of the attributes
 def pullAttrs(url):
 
@@ -29,9 +44,13 @@ def pullAttrs(url):
 
     # make a soup object and parse as html
     soup = BeautifulSoup(r.text, 'html.parser')
+    # print(soup.prettify())
+
     search = soup.find('script', type='application/ld+json')
 
+    # return if the account is public
     if search != None:
+        person['public'] = 1
         contents = json.loads(search.text)
         # populate desired fields
         for key in contents:
@@ -46,6 +65,12 @@ def pullAttrs(url):
                     if key == 'interactionStatistic':
                         person[key] = contents['mainEntityofPage'][key]['userInteractionCount']
 
+    # return if the account is private
+    if search == None:
+        # print(soup.prettify())
+        None
+
+
 if __name__ == '__main__':
 
     # if they entered a handle to look up
@@ -53,11 +78,24 @@ if __name__ == '__main__':
         handle = argv[1]
         url = baseURL + handle
         pullAttrs(url)
-    if argv[2]:
-        print('NAME:\t\t{}'.format(person['name']))
-        print('FOLLOWERS:\t{}'.format(person['interactionStatistic']))
-        print('DESCRIPTION:\t{}'.format(person['description']))
-
     # no handle provided
     else:
-        print('--- NO INSTAGRAM HANDLE PROVIDED ---')
+        print('-~- \\\\ NO INSTAGRAM HANDLE PROVIDED // -~-')
+
+
+    if len(argv) > 2:
+        # enables verbose mode
+        if argv[2]:
+            # the account is public
+            if person['public']:
+                print('NAME:\t\t{}'.format(person['name']))
+                print('FOLLOWERS:\t{}'.format(person['interactionStatistic']))
+                print('DESCRIPTION:\t{}'.format(person['description']))
+            # the account is private
+            else:
+                print('-~- \\\\ USER HAS PRIVATE ACCOUNT // -~-')
+        # no handle provided
+
+    driver = makeDriver()
+    navigate(driver, url)
+    loadAllPictures(driver)
